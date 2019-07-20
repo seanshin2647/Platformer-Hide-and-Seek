470e3f7609138e6c9493b66197eb58ed25729458
Sean Shin <seanshin2647@gmail.com>
2019-07-19 18:45:17 -0600

    Add variables for vertical and horizontal momentum

--- game_classes.py
+++ game_classes.py
@@ -17,3 +17,6 @@ class Player(pygame.sprite.Sprite):
 
         self.rect.x = self.display_width + self.side_length
         self.rect.y = random.randrange(0, self.display_height)
+
+        self.vertical_momentum = 0
+        self.horizontal_momentum = 0
