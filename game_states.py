605bfaaf3babb7bc6d3e89e23d34a32e52a8dae1
Sean Shin <seanshin2647@gmail.com>
2019-07-19 18:27:17 -0600

    Create class Game_State and __init__

--- game_states.py
+++ game_states.py
@@ -16,3 +16,14 @@ class State():
 
     def handle_events(self):
         raise NotImplementedError
+
+class Game_State(State):
+    def __init__(self, display_width, display_height):
+        super().__init__()
+
+        self.all_sprites_list = pygame.sprite.Group()
+        self.all_players_list = pygame.sprite.Group()
+
+        self.player_one = Player(display_width, display_height)
+        self.all_sprites_list.add(self.player_one)
+        self.all_players_list.add(self.player_one)
