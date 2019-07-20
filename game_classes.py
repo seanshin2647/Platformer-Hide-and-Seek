0f3bc803caf39e383aa9a18c3436f65bba0d36f8
Sean Shin <seanshin2647@gmail.com>
2019-07-19 18:12:29 -0600

    Create the player class.
    The player class will later have two children added, one for the seeker
    and one for the hider.

--- game_classes.py
+++ game_classes.py
@@ -1,1 +1,17 @@
 from libraries import *
+
+# TODO: Make a player class and have two child classes for a hider and seeker.
+# Just a square block for now.
+class Player(pygame.sprite.Sprite):
+    def __init__(self, display_width, display_height):
+        super().__init__()
+
+        self.side_length = 30
+
+        self.image = pygame.Surface([self.side_length, self.side_length])
+        self.image.fill(SILVER)
+        self.rect = self.image.get_rect()
+
+        self.rect.x = display_width + self.side_length
+        self.rect.y = random.randrange(0, display_height)
+        
