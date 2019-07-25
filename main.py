1acd0d049f85aee3d756a5f1a5e018faf12ff0de
Sean Shin <seanshin2647@gmail.com>
2019-07-25 16:04:54 -0600

    Add code for display and framrate

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 from game_states import *
+
+pygame.init()
+
+FPS = 60
+DISPLAY_WIDTH = 600
+DISPLAY_HEIGHT = 600
+DISPLAY = pygame.display.set_mode([DISPLAY_WIDTH, DISPLAY_HEIGHT])
+
+clock = pygame.time.Clock()
