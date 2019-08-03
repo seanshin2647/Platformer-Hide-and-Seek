557e9f9c6cbaf04e736a64d7bec91de4a9210381
Sean Shin <seanshin2647@gmail.com>
2019-08-02 23:29:23 -0600

    Make double jumping effective

--- game_classes.py
+++ game_classes.py
@@ -25,7 +25,7 @@ class Player(pygame.sprite.Sprite):
 
     def jump(self):
         if self.jumps_left > 0:
-            self.vertical_momentum += 25
+            self.vertical_momentum = 25
             self.jumps_left -= 1
 
     def left(self):
@@ -56,6 +56,7 @@ class Player(pygame.sprite.Sprite):
         if self.rect.y == self.display_height - self.side_length:
             self.vertical_momentum = 0
         elif self.vertical_momentum > -25:
+                # TODO: Change this to make the jump look smoother.
                 self.vertical_momentum -= 5
 
         # TODO: Change this so that the player will new jumps when they land on a platform.
